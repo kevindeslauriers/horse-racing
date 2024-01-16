b5f2297728162156394e1baeeb06c7138c452fe1
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2024-01-16 11:10:55 -0500

    table code

---
+++ Horse Racing Assignment/src/horseracing/Test.java
@@ -0,0 +1,20 @@
+package horseracing;
+
+public class Test {
+    public static void main(String[] args) {
+        // String temp = "blahblah";
+        // System.out.printf("Hello%10sgood%10sbye", temp, "cha");
+
+        for (int i = 0; i < 5; i++) {
+            String s1 = "" + (int)(Math.random()*10) + 1;
+            String s2 = "" + (int)(Math.random()*10) + 1;
+            String s3 = "" + (int)(Math.random()*10) + 1;
+            String s4 = "" + (int)(Math.random()*10) + 1;
+
+            System.out.println("+-----+-----+-----+-----+");
+            System.out.printf("|%5s|%5s|%5s|%5s|\n", s1, s2, s3, s4);
+        }
+        System.out.println("+-----+-----+-----+-----+");
+
+    }
+}
